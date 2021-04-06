de9cffb42710033178fb31f81a8ef61c6f8ba3a1
psarge2 <54369285+psarge2@users.noreply.github.com>
2021-04-06 18:45:32 -0400

    added Dashboard view into index.js

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,3 +1,5 @@
+import Dashboard from "./views/Dashboard.js";
+
 const navigateTo = url => {
   history.pushState(null, null, url);
   router();
@@ -5,9 +7,9 @@ const navigateTo = url => {
 
 const router = async () => {
   const routes = [
-    { path: "/", view: () => console.log("Viewing Dashboard") },
-    { path: "/posts", view: () => console.log("Viewing Posts") },
-    { path: "/settings", view: () => console.log("Viewing Settings") }
+    { path: "/", view: Dashboard },
+    //{ path: "/posts", view: () => console.log("Viewing Posts") },
+    //{ path: "/settings", view: () => console.log("Viewing Settings") }
   ];
 
   // Test each route for potential match
@@ -26,6 +28,10 @@ const router = async () => {
       isMatch: true
     };
   }
+
+  const view = new match.route.view();
+
+  document.querySelector("#app").innerHTML = await view.getHtml();
 
   console.log(match.route.view());
 };
--- frontend/static/views/Dashboard.js
+++ frontend/static/views/Dashboard.js
@@ -9,7 +9,7 @@ export default class extends AbstractView {
     return `
       <h1>Welcome Back, Paul</h1>
       <p>
-        l
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </p>
       <p>
         <a href="/post" data-link>View recent posts</a>
