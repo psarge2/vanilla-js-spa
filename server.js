484b0c5f8d969fb67ac79babf15feb1d0e978b97
psarge2 <54369285+psarge2@users.noreply.github.com>
2021-04-06 17:41:12 -0400

    added static/js folders and added index.js to js folder and middleware to static path

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ const path = require("path");
 
 const app = express();
 
+//Middleware
+app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")))
+
 app.get("/*", (req, res) => {
-  res.sendFile(path.resolve("frontend", "index.html"));
+  res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
 });
 
 app.listen(process.env.Port || 7000, ()=> console.log("Run RUn RUN..."));
